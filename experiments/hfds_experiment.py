43c4941cf5f0ca4384fef28b9e8095ce6c5f2810
Daniil Kaminskyi <danial.1412@gmail.com>
2025-05-05 10:56:55 +0200

    update hfds experiments

--- experiments/hfds_experiment.py
+++ experiments/hfds_experiment.py
@@ -1,8 +1,7 @@
 import sys
 
-from sympy import sequence
-sys.path.append(".") 
-sys.path.append("./src") 
+sys.path.append(".")
+sys.path.append("./src")
 import csv
 import logging
 import os
@@ -23,8 +22,9 @@ from src.utils.util import get_bounds
 # Configuration
 # CLASSES = range(7)  # Assuming classes 0-6
 # class_pairs = list(permutations(CLASSES, 2))  # All ordered pairs
-SEQUENCE_LENGTHS = [15, 20, 25, 30]
+SEQUENCE_LENGTHS = [15]
 TEST_SIZE = 0.2  # 20% for testing
+BOUND_DEVIATIONS = [0.0, 0.001, 0.002, 0.003, 0.004, 0.005]
 
 # Setup directories
 base_folder = get_experiments_path().joinpath("hdfs")
@@ -36,14 +36,15 @@ CSV_PATH = base_folder.joinpath("results_full.csv")
 SEEDS = [
     114,
     28998,
-    7239,
-    11517,
-    79820,
+    # 7239,
+    # 11517,
+    # 79820,
 ]
 fieldnames = [
     "seed",
     "max_sequence_length",
     "case_name",
+    "deviation",
     "lower_bound",
     "upper_bound",
     "exact_bound",
@@ -81,108 +82,110 @@ for max_sequence_length in SEQUENCE_LENGTHS:
                 max_sequence_length=max_sequence_length,
             )
             exact_bound = len(train_df[train_df.Label == 1]) / len(train_df)
-            lower_start, upper_start = get_bounds(exact_bound, 3)
+            lower_start, upper_start = get_bounds(exact_bound, 2)
 
-            lower = float(lower_start)
-            upper = float(upper_start)
+            for deviation in BOUND_DEVIATIONS:
+                lower = max(0.0, float(lower_start) - deviation)
+                upper = min(1.0, float(upper_start) + deviation)
 
-            boundary_cases = [
-                (lower, upper, "both_bounds"),
-                (lower, None, "lower_only"),
-                (None, upper, "upper_only"),
-            ]
+                boundary_cases = [
+                    (lower, upper, "both_bounds"),
+                    (lower, None, "lower_only"),
+                    (None, upper, "upper_only"),
+                ]
 
-            for lb, ub, case_name in boundary_cases:
-                log_file = str(
-                    base_folder.joinpath(
-                        "logs",
-                        f"{case_name}_{max_sequence_length}_{seed}.log",
-                    )
-                )
-                dfa_file = str(
-                    base_folder.joinpath(
-                        "dfas",
-                        f"{case_name}_{max_sequence_length}_{seed}",
-                    )
-                )
-                log_file_gurobi = str(
-                    base_folder.joinpath(
-                        "gurobi_logs",
-                        f"{case_name}_{max_sequence_length}_{seed}_gurobi.log",
-                    )
-                )
-                logger = setup_logger(log_file)
-
-                row_data = {
-                    "seed": seed,
-                    "max_sequence_length": max_sequence_length,
-                    "case_name": case_name,
-                    "lower_bound": lb,
-                    "upper_bound": ub,
-                    "exact_bound": exact_bound,
-                    "status": "failed",
-                    "error_message": "",
-                }
-
-                try:
-                    dfa, problem = learn_dfa_with_bounds(
-                        sample=train_df.Features.tolist(),
-                        lower_bound=lb,
-                        upper_bound=ub,
-                        alphabet=alphabet,
-                        min_dfa_size=2,
-                        verbose=2,
-                        lambda_l=None,
-                        lambda_s=None,
-                        lambda_p=None,
-                        log_file=log_file_gurobi,
-                    )
-
-                    test_df["Prediction"] = test_df.Features.apply(
-                        lambda x: int(x in dfa)
-                    )
-
-                    row_data.update(
-                        {
-                            "runtime": getattr(problem.model, "Runtime", "N/A"),
-                            "precision": precision_score(
-                                test_df.Label, test_df.Prediction, zero_division=0
-                            ),
-                            "recall": recall_score(
-                                test_df.Label, test_df.Prediction, zero_division=0
-                            ),
-                            "f1": f1_score(
-                                test_df.Label, test_df.Prediction, zero_division=0
-                            ),
-                            "states": len(dfa.states),
-                            "status": "success",
-                        }
-                    )
-
-                    dfa.save_visualized_dfa(dfa_file)
-                    logger.info(
-                        f"Learned DFA saved to {dfa_file}\n"
-                        f"States: {len(dfa.states)}\n"
-                        f"Initital State: {dfa.initial_state}\n"
-                        f"Final States: {dfa.final_states}\n"
-                        f"Alphabet: {dfa.alphabet}\n"
-                        f"Transitions:\n"
-                        f"{"\n".join([f"{from_state} -> {symbol} -> {to_state}"  for from_state, symbols in dfa.transitions.items() for symbol, to_state in symbols.items()])}"
-                    )
-                    logger.info(
-                        f"\nSuccess\n{dfa}\n{classification_report(test_df.Label, test_df.Prediction)}"
-                    )
-
-                except Exception as e:
-                    row_data["error_message"] = str(e)
-                    logger.error(f"Error: {str(e)}", exc_info=True)
-
-                finally:
-                    with open(CSV_PATH, "a") as csvfile:
-                        csv.DictWriter(csvfile, fieldnames=fieldnames).writerow(
-                            row_data
+                for lb, ub, case_name in boundary_cases:
+                    log_file = str(
+                        base_folder.joinpath(
+                            "logs",
+                            f"{case_name}_{max_sequence_length}_{seed}.log",
                         )
-                    logger.handlers.clear()
+                    )
+                    dfa_file = str(
+                        base_folder.joinpath(
+                            "dfas",
+                            f"{case_name}_{max_sequence_length}_{seed}",
+                        )
+                    )
+                    log_file_gurobi = str(
+                        base_folder.joinpath(
+                            "gurobi_logs",
+                            f"{case_name}_{max_sequence_length}_{seed}_gurobi.log",
+                        )
+                    )
+                    logger = setup_logger(log_file)
+
+                    row_data = {
+                        "seed": seed,
+                        "max_sequence_length": max_sequence_length,
+                        "deviation": deviation,
+                        "case_name": case_name,
+                        "lower_bound": lb,
+                        "upper_bound": ub,
+                        "exact_bound": exact_bound,
+                        "status": "failed",
+                        "error_message": "",
+                    }
+
+                    try:
+                        dfa, problem = learn_dfa_with_bounds(
+                            sample=train_df.Features.tolist(),
+                            lower_bound=lb,
+                            upper_bound=ub,
+                            alphabet=alphabet,
+                            min_dfa_size=2,
+                            verbose=2,
+                            lambda_l=None,
+                            lambda_s=None,
+                            lambda_p=None,
+                            log_file=log_file_gurobi,
+                        )
+
+                        test_df["Prediction"] = test_df.Features.apply(
+                            lambda x: int(x in dfa)
+                        )
+
+                        row_data.update(
+                            {
+                                "runtime": getattr(problem.model, "Runtime", "N/A"),
+                                "precision": precision_score(
+                                    test_df.Label, test_df.Prediction, zero_division=0
+                                ),
+                                "recall": recall_score(
+                                    test_df.Label, test_df.Prediction, zero_division=0
+                                ),
+                                "f1": f1_score(
+                                    test_df.Label, test_df.Prediction, zero_division=0
+                                ),
+                                "states": len(dfa.states),
+                                "status": "success",
+                            }
+                        )
+
+                        dfa.save_visualized_dfa(dfa_file)
+                        logger.info(
+                            f"Learned DFA saved to {dfa_file}\n"
+                            f"States: {len(dfa.states)}\n"
+                            f"Initital State: {dfa.initial_state}\n"
+                            f"Final States: {dfa.final_states}\n"
+                            f"Alphabet: {dfa.alphabet}\n"
+                            f"Transitions:\n"
+                            f"{'\n'.join([f'{from_state} -> {symbol} -> {to_state}' for from_state, symbols in dfa.transitions.items() for symbol, to_state in symbols.items()])}"
+                        )
+                        logger.info(
+                            f"\nSuccess\n{dfa}\n{classification_report(test_df.Label, test_df.Prediction)}"
+                        )
+
+                    except Exception as e:
+                        row_data["error_message"] = str(e)
+                        logger.error(f"Error: {str(e)}", exc_info=True)
+
+                    finally:
+                        with open(CSV_PATH, "a") as csvfile:
+                            csv.DictWriter(csvfile, fieldnames=fieldnames).writerow(
+                                row_data
+                            )
+                        logger.handlers.clear()
 
         except Exception as e:
             print(f"Error processing seed ({seed}): {str(e)}")
@@ -194,9 +197,9 @@ df["case_type"] = df.case_name.map(
     {"both_bounds": "TB", "lower_only": "LB", "upper_only": "UB"}
 )
 
-# Aggregate results across all class pairs
+# Visualization
 agg_df = (
-    df.groupby(["case_type", "max_sequence_length"])
+    df.groupby(["case_type", "deviation"])
     .agg(mean_f1=("f1", "mean"), std_f1=("f1", "std"))
     .reset_index()
 )
@@ -242,7 +245,7 @@ plt.title("Average F1 Score vs Acceptance Bound Deviation", pad=15)
 plt.xlabel("Bound Deviation", labelpad=10)
 plt.ylabel("F1 Score", labelpad=10)
 plt.ylim(0, 1.05)
-plt.xlim(-0.1, max(SEQUENCE_LENGTHS) + 0.1)
+plt.xlim(-0.001, max(BOUND_DEVIATIONS) + 0.001)
 plt.grid(True, alpha=0.3)
 
 # Create legend
